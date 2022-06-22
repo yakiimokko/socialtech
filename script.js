8f0e8b418e4569233d2689b250e86a04372af952
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-22 23:53:49 +0000

    script.js

--- script.js
+++ script.js
@@ -57,8 +57,66 @@ $(function(){
     
     //お問い合わせフォームの入力チェック
     function inputCheck() {
-        console.log('呼び出されたよーん');
-    }
-    
-    
+        let result;
+        
+        let message = '';
+        
+        let error = false;
+        
+        //お名前チェック
+        if($('#name').val()== ''){
+            $('#name').css('background-color', '#f79999');
+            error = true;
+            message += 'お名前を入力してください。\n';
+        }
+        else{
+            //エラーなし
+            $('#name').css('background-color', '#fafafa');
+        }
+        
+      // フリガナのチェック
+         if ($('#furigana').val() == '') {
+           // エラーあり
+           $('#furigana').css('background-color', '#f79999');
+           error = true;
+           message += 'フリガナを入力してください。\n';
+         } else {
+           // エラーなし
+           $('#furigana').css('background-color', '#fafafa');
+         }
+     
+         // お問い合わせのチェック
+         if ($('#message').val() == '') {
+           // エラーあり
+           $('#message').css('background-color', '#f79999');
+           error = true;
+           message += 'お問い合わせ内容を入力してください。\n';
+         } else {
+           // エラーなし
+           $('#message').css('background-color', '#fafafa');
+         }
+         
+         // メールアドレスのチェック
+         if ($('#email').val() == '' || $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1)
+         {
+           // エラーあり
+           $('#email').css('background-color', '#f79999');
+           error = true;
+           message += 'メールアドレスが未記入、または「@」「.」が含まれていません。\n';
+         } else {
+           // エラーなし
+           $('#email').css('background-color', '#fafafa');
+         }
+         
+         // 電話番号のチェック（未入力はOK、未入力でない場合は-が必要）
+         if ($('#tel').val() != '' && $('#tel').val().indexOf('-') == -1) {
+           // エラーあり
+           $('#tel').css('background-color', '#f79999');
+           error = true;
+           message += '電話番号に「-」が含まれていません。\n';
+         } else {
+           // エラーなし
+           $('#tel').css('background-color', '#fafafa');
+         }
+      }
 });
