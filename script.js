3aa8b5b53fb08f86aea51b61b550b6d0c0285d96
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-22 23:35:11 +0000

    script.js

--- script.js
+++ script.js
@@ -24,5 +24,41 @@ $(function(){
         arrows: false,
     });
     
+    //AjaxでSTATIC FORMSにデータを送信
+    $('#submit').on('click',function(event) {
+        // formタグによる送信を拒否
+        event.preventDefault();
+        
+        //入力チェックした結果、エラーがあるかないか確認
+        let result = inputCheck();
+    });
+    
+    //フォーカスが外れたとき（blur）にフォームの入力チェックをする
+   $('#name').blur(function () {
+     inputCheck();
+   });
+   $('#furigana').blur(function () {
+     inputCheck();
+   });
+   $('#email').blur(function () {
+     inputCheck();
+   });
+   $('#tel').blur(function () {
+     inputCheck();
+   });
+   $('#message').blur(function () {
+     inputCheck();
+   });
+   $('#agree').click(function () {
+     inputCheck();
+   });
+    
+    
+    
+    //お問い合わせフォームの入力チェック
+    function inputCheck() {
+        console.log('呼び出されたよーん');
+    }
+    
     
 });
